d6a3380b5439ffd5b47146670fbfabfdb49d144c
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-15 13:46:14 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return(
+<div className="app">
+
+<div className="app__body">
+<p>movie react</p>
+</div>
+    </div>
+
+  )
+}
+
+export default App
